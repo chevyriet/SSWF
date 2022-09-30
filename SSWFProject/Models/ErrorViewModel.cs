affb479d88e2402b4544f14fd20541ffa3e1a403
chevyriet <chevyriet040104@gmail.com>
2022-09-30 14:16:29 +0200

    Add project files.

---
+++ SSWFProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SSWFProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
