fd953594a57a3e83ec6514966d8f0fd4e0df36fc
chevyriet <chevyriet040104@gmail.com>
2022-10-05 09:04:11 +0200

    Added onion architecture

---
+++ DomainServices.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace DomainServices.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ EF.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace EF.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
