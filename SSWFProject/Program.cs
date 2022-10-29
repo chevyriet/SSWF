773342142d18a21317a5408567469227136ff3d1
chevyriet <chevyriet040104@gmail.com>
2022-10-29 17:52:00 +0200

    fix

--- SSWFProject/Program.cs
+++ SSWFProject/Program.cs
@@ -46,7 +46,7 @@ builder.Services.AddAuthorization(options =>
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configure the HTTP request pipeline
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
