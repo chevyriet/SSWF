fd251f569d41c4278afc2e3cb3d396931f7303da
chevyriet <chevyriet040104@gmail.com>
2022-10-30 21:07:01 +0100

    fix

--- EB_WebAPI/Program.cs
+++ EB_WebAPI/Program.cs
@@ -30,12 +30,10 @@ builder.Services.AddScoped<EBDbContext>().AddDbContext<EBDbContext>(opts =>
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
 
 app.UseHttpsRedirection();
 
